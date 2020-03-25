46402137ac2e89aff295bd6fe91e0370a67171a6
Brennan Nugent <nugentb@bc.edu>
2020-03-25 17:23:51 -0400

    Basic Sign Up page working

--- djangoLogin/loginPage_test/urls.py
+++ djangoLogin/loginPage_test/urls.py
@@ -20,6 +20,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('accounts/', include('accounts.urls')),
     url('accounts/', include('django.contrib.auth.urls')), # new
     url('', TemplateView.as_view(template_name='home.html'), name='home')
 ]
